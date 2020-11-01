6e02de1565a556f24a16dcd416ccebf59ea0c67c
ranveerrajput <ranveerrajput351@gmail.com>
2020-11-01 12:16:56 +0530

    Lunch OCT 2020

---
+++ ANDPlusOR.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main(){
+    ll ntc;
+    cin>>ntc;
+    while(ntc--){
+        ll n;
+        cin>>n;
+        cout<<1<<" "<<n-1<<endl;
+    }
+    
+    
+    return 0;
+}
